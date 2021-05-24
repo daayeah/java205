9d549ce17be6b02aafc28ab55e9f5e72336270ee
daayeah <daayeah515@gmail.com>
2021-05-24 16:59:10 +0900

    CHAPTER01
    1챕터 연습문제

---
+++ java_project/MyProject/src/ch01/Calculator.java
@@ -0,0 +1,24 @@
+package ch01;
+
+public class Calculator {
+	void plus(int n1, int n2) {
+		System.out.println(n1 + n2);
+	}
+	void minus(int n1, int n2) {
+		System.out.println(n1 - n2);
+	}
+	void multi(int n1, int n2) {
+		System.out.println(n1 * n2);
+	}
+	void div(int n1, int n2) {
+		System.out.println(n1 / n2);
+	}
+	
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		cal.plus(10, 20);
+		cal.minus(20, 10);
+		cal.multi(2, 5);
+		cal.div(10, 3);
+	}
+}
---
+++ java_project/MyProject/src/ch01/Member.java
@@ -0,0 +1,7 @@
+package ch01;
+
+public class Member {
+	public static void main(String[] args) {
+		System.out.println("안녕하세요. 정다예입니다.");
+	}
+}
