bc6522e959572be5bc0de4045cf922ba7b0a4336
daayeah <daayeah515@gmail.com>
2021-05-25 12:25:03 +0900

    CHAPTER03
    3챕터 연습문제

---
+++ java_project/MyProject/src/ch03/Calculator.java
@@ -0,0 +1,52 @@
+package ch03;
+
+import java.util.Scanner;
+
+public class Calculator {
+	void plus(int num1, int num2) {
+		System.out.println(" 덧셈  : " + num1 + "+" + num2 + "=" + (num1 + num2));
+	}
+	void minus(int num1, int num2) {
+		System.out.println(" 뺄셈  : " + num1 + "-" + num2 + "=" + (num1 - num2));
+	}
+	void multi(int num1, int num2) {
+		System.out.println(" 곱셈  : " + num1 + "*" + num2 + "=" + (num1 * num2));
+	}
+	void div(int num1, int num2) {
+		System.out.println("나눗셈 : " + num1 + "/" + num2 + "=" + (num1 / num2));
+	}
+	void circum(int radius, double pi) {
+		System.out.println("원의 둘레 : " + (2 * pi * radius));
+	}
+	void area(int radius, double pi) {
+		System.out.println("원의 넓이 : " + (pi * radius * radius));
+	}
+
+	public static void main(String[] args) {
+		int num1, num2, radius;
+		double pi = 3.14;
+
+		System.out.print("첫번째 숫자 입력 : ");
+		Scanner sc = new Scanner(System.in);
+		num1 = sc.nextInt();						
+
+		System.out.print("두번째 숫자 입력 : ");
+		Scanner sc2 = new Scanner(System.in);
+		num2 = sc2.nextInt();	
+		
+		Calculator cal = new Calculator();
+		cal.plus(num1, num2);
+		cal.minus(num1, num2);
+		cal.multi(num1, num2);
+		cal.div(num1, num2);
+		System.out.println();
+		
+		System.out.print("실수 반지름 입력 : ");
+		Scanner sc3 = new Scanner(System.in);
+		radius = sc3.nextInt();
+		
+		Calculator cal2 = new Calculator();
+		cal2.circum(radius, pi);
+		cal2.area(radius, pi);
+	}
+}
