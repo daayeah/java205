f7a98203ca9ac29db39c72b5cb6bfb6182f1bdea
daayeah <daayeah515@gmail.com>
2021-05-25 10:52:42 +0900

    Update Member.java

--- java_project/MyProject/src/ch02/Member.java
+++ java_project/MyProject/src/ch02/Member.java
@@ -10,6 +10,12 @@ public class Member {
 		System.out.println("이름 : " + name);
 		System.out.println("나이 : " + age);
 		System.out.println("키 : " + height);
-		System.out.println("책 보유 여부 : " + hasBook);		
+		System.out.println("책 보유 여부 : " + hasBook);	
+		
+		if(hasBook) {
+			System.out.println("책을 갖고 있습니다.");
+		} else {
+			System.out.println("책을 갖고 있지 않습니다.");
+		}
 	}
 }
