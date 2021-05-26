d45b9d4908c85041c46c9668db69100ff54a7298
daayeah <daayeah515@gmail.com>
2021-05-26 18:38:40 +0900

    CHAPTER07

--- java_project/MyProject/src/ch07/Female.java
+++ java_project/MyProject/src/ch07/Female.java
@@ -6,7 +6,9 @@ class Female extends Person {
 		super.idNo = "080101-4000000";
 		super.Hello();
 		
-		if(super.gender() == '2' || super.gender() == '4') {
+		if(super.gender() == '1' || super.gender() == '3') {
+			System.out.println("저의 성별은 남성입니다.");
+		} else if(super.gender() == '2' || super.gender() == '4') {
 			System.out.println("저의 성별은 여성입니다.");
 		}
 	}
--- java_project/MyProject/src/ch07/Male.java
+++ java_project/MyProject/src/ch07/Male.java
@@ -5,9 +5,11 @@ class Male extends Person {
 		super.name = "익명1";
 		super.idNo = "900101-1000000";
 		super.Hello();
-		
+
 		if(super.gender() == '1' || super.gender() == '3') {
 			System.out.println("저의 성별은 남성입니다.");
+		} else if(super.gender() == '2' || super.gender() == '4') {
+			System.out.println("저의 성별은 여성입니다.");
 		}
 	}
 }
