be46e983ea3fbd7a7c76eb4cc2aa08c5e965f8c7
daayeah <daayeah515@gmail.com>
2021-05-26 14:15:45 +0900

    Update Student.java

--- java_project/MyProject/src/ch06/Student.java
+++ java_project/MyProject/src/ch06/Student.java
@@ -2,99 +2,96 @@ package ch06;
 
 public class Student {
 	private String name;
-	private int korsum = 0;
-	private int engsum = 0;
-	private int mathsum = 0;
-	private double koravr = 0;
-	private double engavr = 0;
-	private double mathavr = 0;
-	
-	private int[] korScore = {80, 65, 89, 70, 99, 45, 88, 50, 98, 87};
-	private int[] engScore = {77, 68, 79, 95, 100, 85, 80, 50, 79, 45};
-	private int[] mathScore = {40, 90, 88, 75, 95, 66, 70, 90, 100, 97};
+	private int korScore;
+	private int engScore;
+	private int mathScore;
+
+	Student(String name, int korScore,
+			int engScore, int mathScore){
+		this.name = name;
+		this.korScore = korScore;
+		this.engScore = engScore;
+		this.mathScore = mathScore;		
+	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 	public String getName() {
-		return this.name;
+		return name;
 	}
 
-	void totalScore() {
-		for(int i = 0; i < korScore.length; i++) {
-			korsum += korScore[i];	
-		}
-		for(int j = 0; j < engScore.length; j++) {
-			engsum += engScore[j];
-		}
-		for(int k = 0; k < mathScore.length; k++) {
-			mathsum += mathScore[k];
-		}
+	public void setKorScore(int korScore) {
+		this.korScore = korScore;
+	}
+	public int getKorScore() {
+		return korScore;
 	}
 
-	public void setTotalKor(int korsum) {
-		this.korsum = korsum;
+	public void setEngScore(int engScore) {
+		this.engScore = engScore;
 	}
-	public int getTotalKor() {
-		return this.korsum;
-	}
-
-	public void setTotalEng(int engsum) {
-		this.engsum = engsum;
-	}
-	public int getTotalEng() {
-		return this.engsum;
+	public int getEngScore() {
+		return engScore;
 	}
 
-	public void setTotalMath(int mathsum) {
-		this.mathsum = mathsum;
+	public void setMathScore(int mathScore) {
+		this.mathScore = mathScore;
 	}
-	public int getTotalMath() {
-		return this.mathsum;
+	public int getMathScore() {
+		return mathScore;
 	}
-	
-	void avrScore() {
-		koravr = korsum / 10.0;
-		engavr = engsum / 10.0;
-		mathavr = mathsum / 10.0;
+
+	public int getTotalSum() {
+		return korScore + engScore + mathScore;
 	}
-	
-	public void setAvrKor(double koravr) {
-		this.koravr = koravr;
+	public double getAvrScore() {
+		double avr = (korScore + engScore + mathScore) / 3;
+		return Math.round(avr * 10) / 10.0;
 	}
-	public double getAvrKor() {
-		return this.koravr;
+
+	void scoreInfo() {
+		System.out.println("이름 : " + name);
+		System.out.println("국어 : " + korScore);
+		System.out.println("영어 : " + engScore);
+		System.out.println("수학 : " + mathScore);
+		System.out.println("총점 : " + getTotalSum());
+		System.out.println("평균 : " + getAvrScore());
+		System.out.println();
 	}
-	
-	public void setAvrEng(double engavr) {
-		this.engavr = engavr;
-	}
-	public double getAvrEng() {
-		return this.engavr;
-	}
-	
-	public void setAvrMath(double mathavr) {
-		this.mathavr = mathavr;
-	}
-	public double getAvrMath() {
-		return this.mathavr;
-	}
-	
 
 	public static void main(String[] args) {
-		Student stu = new Student();
-		stu.totalScore();
-		stu.avrScore();
-		System.out.println("<국어>");
-		System.out.println("총점 : " + stu.getTotalKor());
-		System.out.println("평균 : " + stu.getAvrKor());
+		int korSum = 0;
+		int engSum = 0;
+		int mathSum = 0;
 
+		Student[] stu = new Student[10];		
+		stu[0] = new Student("A", 100, 80, 96);
+		stu[1] = new Student("B", 88, 90, 75);
+		stu[2] = new Student("C", 90, 68, 77);
+		stu[3] = new Student("D", 89, 76, 80);
+		stu[4] = new Student("E", 87, 65, 75);
+		stu[5] = new Student("F", 90, 93, 100);
+		stu[6] = new Student("G", 80, 54, 86);
+		stu[7] = new Student("H", 65, 82, 80);
+		stu[8] = new Student("I", 93, 80, 64);
+		stu[9] = new Student("J", 76, 50, 98);
+
+		for(int i = 0; i < stu.length; i++) {
+			stu[i].scoreInfo();
+			korSum += stu[i].korScore;
+			engSum += stu[i].engScore;
+			mathSum += stu[i].mathScore;
+		}
+
+		System.out.println("<국어>");
+		System.out.println("총점 : " + korSum);
+		System.out.println("평균 : " + (double)korSum / stu.length);
 		System.out.println("\n<영어>");
-		System.out.println("총점 : " + stu.getTotalEng());
-		System.out.println("평균 : " + stu.getAvrEng());
-		
+		System.out.println("총점 : " + engSum);
+		System.out.println("평균 : " + (double)engSum / stu.length);
 		System.out.println("\n<수학>");
-		System.out.println("총점 : " + stu.getTotalMath());
-		System.out.println("평균 : " + stu.getAvrMath());
+		System.out.println("총점 : " + mathSum);
+		System.out.println("평균 : " + (double)mathSum / stu.length);
 	}
 }
