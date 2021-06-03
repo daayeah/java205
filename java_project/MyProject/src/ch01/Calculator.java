fd744febeadd83906d254289b0fc9cdab17834ce
daayeah <daayeah515@gmail.com>
2021-06-04 04:19:21 +0900

    Update Calculator.java

--- java_project/MyProject/src/ch01/Calculator.java
+++ java_project/MyProject/src/ch01/Calculator.java
@@ -4,16 +4,19 @@ public class Calculator {
 	void plus(int n1, int n2) {
 		System.out.println(n1 + n2);
 	}
+
 	void minus(int n1, int n2) {
 		System.out.println(n1 - n2);
 	}
+
 	void multi(int n1, int n2) {
 		System.out.println(n1 * n2);
 	}
+
 	void div(int n1, int n2) {
 		System.out.println(n1 / n2);
 	}
-	
+
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
 		cal.plus(10, 20);
