4e11782db291e67548c4cfcdc033bea8dd117efc
daayeah <daayeah515@gmail.com>
2021-06-03 23:59:59 +0900

    Update Member.java

--- java_project/MyProject/src/ch02/Member.java
+++ java_project/MyProject/src/ch02/Member.java
@@ -5,14 +5,14 @@ public class Member {
 		String name = "익명";
 		int age = 20;
 		double height = 160.5;
-		boolean hasBook = true;
-		
+		boolean hasBook = false;
+
 		System.out.println("이름 : " + name);
 		System.out.println("나이 : " + age);
 		System.out.println("키 : " + height);
-		System.out.println("책 보유 여부 : " + hasBook);	
-		
-		if(hasBook) {
+		System.out.println("책 보유 여부 : " + hasBook);
+
+		if (hasBook) {
 			System.out.println("책을 갖고 있습니다.");
 		} else {
 			System.out.println("책을 갖고 있지 않습니다.");
