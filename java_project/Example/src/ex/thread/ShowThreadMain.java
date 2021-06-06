798df1959060ae34b66d9e1619d539364252affe
daayeah <daayeah515@gmail.com>
2021-06-07 05:56:59 +0900

    Create ShowThreadMain.java

---
+++ java_project/Example/src/ex/thread/ShowThreadMain.java
@@ -0,0 +1,13 @@
+package ex.thread;
+
+public class ShowThreadMain {
+	public static void main(String[] args) {
+		// 스레드 인스턴스 생성
+		ShowThread t1 = new ShowThread("스레드1");
+		ShowThread t2 = new ShowThread("	스레드2");
+
+		// 스레드 실행
+		t1.start();
+		t2.start();
+	}
+}
