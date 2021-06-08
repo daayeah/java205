220a434f72b8ade996a29e50fadb0b318e5f29e9
daayeah <daayeah515@gmail.com>
2021-06-08 13:25:52 +0900

    Create HighFriend.java

---
+++ java_project/Example/src/ex/inheritance/phone2/HighFriend.java
@@ -0,0 +1,25 @@
+package ex.inheritance.phone2;
+
+public class HighFriend extends Friend {
+	String work;
+
+	public HighFriend(String name, String phoneNum, String address, String work) {
+		super(name, phoneNum, address);
+		this.work = work;
+	}
+
+	@Override
+	public void allInfo() {
+		super.allInfo();
+		System.out.println("직    업 : " + work);
+		System.out.println("------------------------------");
+	}
+
+	@Override
+	public void basicInfo() {
+		System.out.println("이    름 : " + name);
+		System.out.println("전화번호 : " + phoneNum);
+		System.out.println("------------------------------");
+	}
+
+}
