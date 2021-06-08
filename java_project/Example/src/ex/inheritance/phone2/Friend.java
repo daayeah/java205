66de0953dfda7a862d15c955b06d31f5664777ca
daayeah <daayeah515@gmail.com>
2021-06-08 13:25:47 +0900

    Create Friend.java

---
+++ java_project/Example/src/ex/inheritance/phone2/Friend.java
@@ -0,0 +1,22 @@
+package ex.inheritance.phone2;
+
+public abstract class Friend implements ShowInfo {
+	String name;
+	String phoneNum;
+	String address;
+
+	public Friend(String name, String phoneNum, String address) {
+		super();
+		this.name = name;
+		this.phoneNum = phoneNum;
+		this.address = address;
+	}
+
+	public void allInfo() {
+		System.out.println("이    름 : " + name);
+		System.out.println("전화번호 : " + phoneNum);
+		System.out.println("주    소 : " + address);
+	}
+
+	public abstract void basicInfo();
+}
