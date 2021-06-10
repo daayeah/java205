2ea8357d52527e6c96ce1a68e468d4e578727b3b
daayeah <daayeah515@gmail.com>
2021-06-10 17:40:58 +0900

    Update select_exam2.sql

--- sql/select_exam2.sql
+++ sql/select_exam2.sql
@@ -6,7 +6,6 @@ FROM
     book
 WHERE
     bookid = 1;
-   
     
 -- (2) 가격이 20,000원 이상인 도서의 이름
 SELECT
@@ -31,7 +30,6 @@ FROM
     orders
 WHERE
     custid = 1;
-
 ​
 -- 2. 마당서점의 운영자와 경영자가 요구하는 다음 질문에 대해 SQL문을 작성하시오.
 -- (1) 마당서점 도서의 총 개수
@@ -47,22 +45,44 @@ FROM
     book;
 
 -- (3) 모든 고객의 이름, 주소
-SELECT name, address FROM customer ;
-
+SELECT
+    name,
+    address
+FROM
+    customer;
+    
 -- (4) 2014년 7월 4일 ~ 7월 7일 사이에 주문 받은 도서의 주문번호
-
- SELECT ORDERID FROM ORDERS WHERE ORDERDATE BETWEEN '14/07/04' AND '14/07/07' ;
-
+SELECT
+    orderid
+FROM
+    orders
+WHERE
+    orderdate BETWEEN '14/07/04' AND '14/07/07';
+    
 -- (5) 2014년 7월 4일 ~ 7월 7일 사이에 주문 받은 도서를 제외한 도서의 주문번호
-
- SELECT ORDERID FROM
-ORDERS WHERE ORDERDATE NOT BETWEEN '14/07/04' AND '14/07/07' ;
+SELECT
+    orderid
+FROM
+    orders
+WHERE
+    orderdate NOT BETWEEN '14/07/04' AND '14/07/07';
 
 -- (6) 성이 ‘김’씨인 고객의 이름과 주소
-
- SELECT NAME , ADDRESS FROM CUSTOMER WHERE NAME LIKE '김%' ;
-
+SELECT
+    name,
+    address
+FROM
+    customer
+WHERE
+    name LIKE '김%';
+    
 -- (7) 성이 ‘김’씨이고 이름이 ‘아’로 끝나는 고객의 이름과 주소
+SELECT
+    name,
+    address
+FROM
+    customer
+WHERE
+    name LIKE '김%아';
 
- SELECT NAME ,
-ADDRESS FROM CUSTOMER WHERE NAME LIKE '김%아' ; COMMIT ;
+COMMIT;
