e5a247bf72e40d4eab62da69689f441c08b21768
daayeah <daayeah515@gmail.com>
2021-09-05 12:57:01 +0900

    Create IndexController.java

---
+++ spring_project/#old/OpenProject/src/main/java/com/bitcamp/op/IndexController.java
@@ -0,0 +1,23 @@
+package com.bitcamp.op;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public void index() {
+	}
+
+	@RequestMapping("/index2")
+	public void index2() {
+	}
+
+	@RequestMapping("/")
+	public String index1() {
+		System.out.println("/ 진입");
+		return "redirect:/index";
+	}
+
+}
